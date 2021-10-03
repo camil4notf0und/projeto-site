898eb99f9a4836994a99ea7aa0f3f73737316473
Felipe Laskos <felipelaskosmatheus@gmail.com>
2021-10-02 22:38:16 -0300

    ?

--- assets/js/cabecalho.js
+++ assets/js/cabecalho.js
@@ -1,17 +1,9 @@
-var cabecalho = document.querySelector(".cabecalho");
-var principal = document.querySelector(".principal");
+const iconesCabecalho = document.querySelectorAll(".navegacao--configs .fas");
 
-var scroll;
-window.addEventListener("scroll", function (event) {
-    scroll = this.scrollY;
-	if(scroll>500) { cabecalho.style.transition = "opacity 1s, top  1s"};
-	if(scroll<=500) { cabecalho.style.top = "-70px"; }
-	if(scroll>510) { cabecalho.style.top = "0"; }
-	if(scroll>=200) {
-		cabecalho.style.position = "fixed";
-		principal.style.margin = "61.89px 0 0 0";
-	}else {
-		cabecalho.style.position = "static";
-		principal.style.margin = "0";
-	}
+const widthCabecalho = document.getElementsByTagName("header")[0].clientWidth;
+
+const tamanhoIcones = widthCabecalho * 0.4725;
+
+iconesCabecalho.forEach((icone) => {
+	icone.style.fontSize = `${tamanhoIcones}px`;
 });
